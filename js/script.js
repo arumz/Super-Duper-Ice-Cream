df0230da21b69ddcae552270f3cade5fd55ab2f2
Andrew Roembke <aroembke21@gmail.com>
2019-04-19 18:18:03 -0400

    finishing up contact page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 $.getJSON('./json/data.json', function(data){
-    console.log(data.header);
+    //home
     $('.title').html(data.header);
     $('#tagline').html(data.tagline);
+
+    //flavors
+    $('.price').each(function(idx){
+        $(this).html("Price: $ " + data.flavors[idx].cost);
+    });
+
+    $('.ice-cream-title').each(function(idx){
+        $(this).html(data.flavors[idx].flavor)
+    })
+
+    //contact
+    $(".form").on("submit",function(e) {
+        e.preventDefault(); // cancel submission
+        window.location.replace("/");
+    });
+
 
 });
